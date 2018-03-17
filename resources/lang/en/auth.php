a99ff8de99323447fd29f4a0e2c355a7dc860fb2
wy <1137917270@qq.com>
2018-03-17 20:02:02 +0800

    注释注册链接

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => '登录失败',
+    'throttle' => '登录次数过多，请在几秒后重试',
 
 ];
